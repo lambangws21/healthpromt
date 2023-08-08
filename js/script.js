e65f2c4ae03ffe35f67015816b05151413e8ced8
unknown <lambangws21@gmail.com>
2023-08-08 16:25:18 +0700

    menambahkan js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,17 @@ const navbarNav = document.querySelector(".navbar-nav")
 
 // Ketika Halaman di klik
 document.querySelector("#humberger-menu").
-onclick = () =>{
-    navbarNav.classList.toggle("active")
-}
+    onclick = () => {
+        navbarNav.classList.toggle("active")
+    }
+
+// Klik di luar sidebar untuk menghilangkan nav
+
+const hamburger = document.querySelector("#humberger-menu");
+
+document.addEventListener("click", function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target))
+    {
+        navbarNav.classList.remove("active");
+    }
+})
