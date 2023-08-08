987a0d687ece0e487a4e5c740b6de72936490f50
unknown <lambangws21@gmail.com>
2023-08-08 16:06:12 +0700

    first uploaded

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav")
+
+
+// Ketika Halaman di klik
+document.querySelector("#humberger-menu").
+onclick = () =>{
+    navbarNav.classList.toggle("active")
+}
