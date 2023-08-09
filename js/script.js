b9fa70c9698bbefdcac55f9375e2899276a79273
unknown <lambangws21@gmail.com>
2023-08-09 19:54:47 +0700

    menambahkan javascrip data

--- js/script.js
+++ js/script.js
@@ -1,21 +1,40 @@
-// Toggle class active
+// Toggle class active humberger menu
 
 const navbarNav = document.querySelector(".navbar-nav")
 
 
+// Toggle active search menu
+const searchForm = document.querySelector(".search-form")
+const searchBox = document.querySelector("#seacrh-box")
+
+document.querySelector("#search-button").
+    onclick = (e) => {
+        searchForm.classList.toggle("active");
+        searchBox.focus();
+        e.preventDefault();
+    }
 // Ketika Halaman di klik
 document.querySelector("#humberger-menu").
     onclick = () => {
         navbarNav.classList.toggle("active")
     }
 
-// Klik di luar sidebar untuk menghilangkan nav
+// Klik di luar element
 
 const hamburger = document.querySelector("#humberger-menu");
+
+const searchButton = document.querySelector("#search-button")
 
 document.addEventListener("click", function (e) {
     if (!hamburger.contains(e.target) && !navbarNav.contains(e.target))
     {
         navbarNav.classList.remove("active");
     }
-})
+});
+
+document.addEventListener("click", function (e) {
+    if (!searchForm.contains(e.target) && !searchForm.contains(e.target))
+    {
+        searchForm.classList.remove("active");
+    }
+});
